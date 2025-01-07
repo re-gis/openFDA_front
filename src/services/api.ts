864c7640fdd782fdf66c105568353a19a893e92b
re-gis <irumvaregisdmc@gmail.com>
2025-01-07 21:00:43 +0200

    [fix]:fixing backend url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5000/api/v1",
+  baseURL: "https://openfda-api.onrender.com/api/v1",
 });
 
 export default instance;
